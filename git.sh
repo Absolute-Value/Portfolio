c18c89ecdf9779a27ec5f16a9fc254decca427bf
Absolute-Value <ji.kei1220@gmail.com>
2022-07-10 01:27:25 +0900

    Update

---
+++ git.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+git add .
+if [ $# -ne 0 ]; then
+    git commit -m "Update"
+else
+    git commit -m "$1"
+fi
+git push
+cd _site
+git add .
+if [ $# -ne 0 ]; then
+    git commit -m "Update"
+else
+    git commit -m "$1"
+fi
+git push
+cd ../
