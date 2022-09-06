addfe7d922ec6658ce8ac3b5799b1e7630c7e6e8
Absolute_Value <ji.kei1220@gmail.com>
2022-09-06 14:05:44 +0900

    Update

--- push.ps1
+++ push.ps1
@@ -1,9 +1,17 @@
 git add .
-git commit -m $Args[0]
+If($args.Length > 0){
+    git commit -m $Args[0]
+}Else{
+    git commit -m "Update"
+}
 git push
 
 cd _site
 git add .
-git commit -m $Args[0]
+If($args.Length > 0){
+    git commit -m $Args[0]
+}Else{
+    git commit -m "Update"
+}
 git push
 cd ../
