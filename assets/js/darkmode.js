27a9e9a00dc8c44c8bbb8178720e475b12f658c4
jikuya <jikuya@cv.info.gifu-u.ac.jp>
2022-12-26 21:09:10 +0900

    fa-moon-sun

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -20,8 +20,18 @@ function change(mode) {
   if (mode) {
     document.body.classList.remove("light-theme");
     document.body.classList.add("dark-theme");
+
+    let element = document.getElementById('sun_moon');
+    if ("fa fa-moon-o" == element.className){
+      element.classList.replace("fa-moon-o", "fa-sun-o");
+    }
   } else {
     document.body.classList.remove("dark-theme");
     document.body.classList.add("light-theme");
+
+    let element = document.getElementById('sun_moon');
+    if ("fa fa-sun-o" == element.className){
+      element.classList.replace("fa-sun-o", "fa-moon-o");
+    }
   }
 }
