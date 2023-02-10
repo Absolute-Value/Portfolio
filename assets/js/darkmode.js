1b7813cb836aa8938564b2006677f4865a713a6a
Absolute-Value <ji.kei1220@gmail.com>
2023-02-10 23:34:02 +0900

    ページを移行してもダークモードを維持するように変更

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -4,6 +4,14 @@ if (window.matchMedia){
   if (window.matchMedia('(prefers-color-scheme: dark)').matches){
     dark_mode = true;
   }
+
+  //ローカルストレージ判定
+  if(localStorage.getItem('dark-mode-settings')==='dark') {
+    dark_mode = true;
+  }else if (localStorage.getItem('dark-mode-settings')==='light') {
+    dark_mode = false;
+  }
+
   change(dark_mode);
 }
  
@@ -20,6 +28,7 @@ function change(mode) {
   if (mode) {
     document.body.classList.remove("light-theme");
     document.body.classList.add("dark-theme");
+    localStorage.setItem('dark-mode-settings', 'dark');
 
     let element = document.getElementById('sun_moon');
     if ("fa fa-moon-o" == element.className){
@@ -28,6 +37,7 @@ function change(mode) {
   } else {
     document.body.classList.remove("dark-theme");
     document.body.classList.add("light-theme");
+    localStorage.setItem('dark-mode-settings', 'light');
 
     let element = document.getElementById('sun_moon');
     if ("fa fa-sun-o" == element.className){
